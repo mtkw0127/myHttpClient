006231537c6eee929ff5d3c84ac6c74c57f243a9
mtkw0127 <maccho.chekera@gmail.com>
2025-06-14 23:21:10 +0900

    Add transferEncoding chunked

--- app/src/main/java/io/github/mtkw0127/klient/Server.kt
+++ app/src/main/java/io/github/mtkw0127/klient/Server.kt
@@ -1,12 +1,16 @@
 package io.github.mtkw0127.klient
 
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.request.receiveText
 import io.ktor.server.response.respondText
+import io.ktor.server.response.respondTextWriter
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import kotlinx.coroutines.delay
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
@@ -14,8 +18,19 @@ fun main() {
             get("/") {
                 call.respondText("Hello, World!")
             }
+            get("/chunked") {
+                call.response.headers.append(HttpHeaders.TransferEncoding, "chunked")
+                call.respondTextWriter(contentType = ContentType.Text.Plain) {
+                    val messages = listOf("Hello", "World", "This", "is", "chunked", "response")
+                    for (msg in messages) {
+                        write(msg)
+                        flush()
+                        delay(500)
+                    }
+                }
+            }
             post("/echo") {
-                val body =  call.receiveText()
+                val body = call.receiveText()
                 call.respondText("Echo: $body")
             }
         }
